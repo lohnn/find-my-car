7f70115595d147ada31df1c1d7359d649a81c708
Johannes Löhnn <johannes.lohnn@infomaker.com>
2016-04-03 19:34:18 +0200

    Continued styling of car actions

--- mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
+++ mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
@@ -1,6 +1,7 @@
 package se.designcoach.findmycar.fragment
 
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,22 +15,28 @@ import se.designcoach.findmycar.model.Car
 
 class CarActionsFragment : Fragment() {
     companion object {
+        val ARG_CAR_NAME = "carName"
         fun newInstance(car: Car): CarActionsFragment {
             val myFragment = CarActionsFragment();
 
             val args = Bundle();
-            //        args.putInt("someInt", someInt);
+            args.putString(ARG_CAR_NAME, car.name);
             myFragment.arguments = args;
 
             return myFragment;
         }
     }
 
+    var carName: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        carName = arguments.getString(ARG_CAR_NAME)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_car_actions, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_car_actions, container, false)
+        //        (view.findViewById(R.id.car_name) as TextView).text = carName
+        (view.findViewById(R.id.carActions_collapsing_toolbar) as CollapsingToolbarLayout).title = carName
+        return view
     }
 }
