92915a2b1d57e2709ca28d92083fdb4fe1385392
lohnn <jlohnn@gmail.com>
2016-04-04 20:54:35 +0200

    Redesigning car actions

--- mobile/src/main/java/se/designcoach/findmycar/MainActivity.kt
+++ mobile/src/main/java/se/designcoach/findmycar/MainActivity.kt
@@ -8,14 +8,20 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.ActionBarActivity
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import se.designcoach.findmycar.adapter.MainCarAdapter
 import se.designcoach.findmycar.fragment.CarActionsFragment
 import se.designcoach.findmycar.model.BluetoothDevice
@@ -27,7 +33,7 @@ import java.util.*
  * Created by lohnn-macPro on 31/03/16.
  */
 
-class MainActivity : FragmentActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         val TAG = "MainActivity"
         val ARG_CAR_POSITION = "CarPosition"
@@ -35,23 +41,30 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     }
 
     private var mMap: GoogleMap? = null
-    private var carPosition: LatLng? = null
     private var carActionsFragment: CarActionsFragment? = null
+    private var slidingUpPanel: SlidingUpPanelLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val args = intent.extras
-        carPosition = args?.getParcelable<LatLng?>(ARG_CAR_POSITION)
-        if (carPosition == null) carPosition = LatLng(56.684238, 16.320195)
+
         setContentView(R.layout.activity_main)
         //Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        //Add some static cars to the list
+        val carPosition = LatLng(56.684238, 16.320195)
         val golfen = Car("Golfen", emptyArray<BluetoothDevice>())
         val uppen = Car("Uppen", emptyArray())
         uppen.lastSeen = LastSeenPosition(Date(), carPosition!!)
         val cars = arrayOf(golfen, uppen)
+
+        //Toolbar
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        //SlidingUpPanel (car list)
+        slidingUpPanel = findViewById(R.id.sliding_layout) as SlidingUpPanelLayout?
 
         val mainContent = findViewById(R.id.main_content)
         val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
@@ -62,7 +75,7 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
                 carActionsFragment = CarActionsFragment.newInstance(car)
                 val ft = supportFragmentManager.beginTransaction()
-                ft.add(mainContent.id, carActionsFragment).addToBackStack(null).commit()
+                ft.add(mainContent!!.id, carActionsFragment).addToBackStack(null).commit()
 
                 //                if(car.lastSeen != null){
                 //                    mMap!!.addMarker(MarkerOptions().position(car.lastSeen!!.position).title(getString(R.string.heres_your_car)))
@@ -71,6 +84,20 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
             }
         })
         recyclerView.addItemDecoration(DividerItemDecoration(this, null))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_settings -> {
+                Log.d(TAG, "Want to open settings")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -82,6 +109,10 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     }
 
     override fun onBackPressed() {
+        if (slidingUpPanel?.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            slidingUpPanel!!.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
+            return
+        }
         super.onBackPressed()
     }
 
--- mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
+++ mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
@@ -3,9 +3,7 @@ package se.designcoach.findmycar.fragment
 import android.os.Bundle
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import se.designcoach.findmycar.R
 import se.designcoach.findmycar.model.Car
 
@@ -31,6 +29,7 @@ class CarActionsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         carName = arguments.getString(ARG_CAR_NAME)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
