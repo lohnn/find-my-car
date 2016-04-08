34174d4ebbd42fb977396da5e77fb83687a7254c
Johannes Löhnn <jlohnn@gmail.com>
2016-04-09 00:12:57 +0200

    Started working with color in AppBars with Palettes.

--- mobile/src/main/java/se/designcoach/findmycar/activity/EditCarActivity.kt
+++ mobile/src/main/java/se/designcoach/findmycar/activity/EditCarActivity.kt
@@ -2,6 +2,9 @@ package se.designcoach.findmycar.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import se.designcoach.findmycar.R
 
 /**
@@ -12,5 +15,14 @@ class EditCarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_car)
+
+        //Setup toolbar
+        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
+        title = getString(R.string.title_activity_edit_car)
+        val carImageView = findViewById(R.id.imageView) as ImageView
+        Glide.with(applicationContext)
+                .load(R.drawable.oldtimer)
+                .centerCrop()
+                .into(carImageView)
     }
 }
--- mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
+++ mobile/src/main/java/se/designcoach/findmycar/fragment/CarActionsFragment.kt
@@ -1,8 +1,11 @@
 package se.designcoach.findmycar.fragment
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.graphics.Palette
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -10,9 +13,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import se.designcoach.findmycar.DividerItemDecoration
-import se.designcoach.findmycar.activity.MainActivity
 import se.designcoach.findmycar.R
+import se.designcoach.findmycar.activity.MainActivity
 import se.designcoach.findmycar.adapter.CarActionsAdapter
 import se.designcoach.findmycar.dal.DataManager
 import se.designcoach.findmycar.model.Car
@@ -24,6 +29,7 @@ import se.designcoach.findmycar.model.CarAction
 
 class CarActionsFragment : Fragment() {
     companion object {
+        val TAG = "CarActionsFragment"
         val ARG_CAR_ID = "carName"
         fun newInstance(carId: Int): CarActionsFragment {
             val myFragment = CarActionsFragment();
@@ -47,13 +53,36 @@ class CarActionsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_car_actions, container, false)
 
         //TODO: add v7 palette library for setting toolbar color
+        val toolbar = view.findViewById(R.id.carActions_collapsing_toolbar) as CollapsingToolbarLayout
         val carImageView = view.findViewById(R.id.imageView) as ImageView
-        Glide.with(this)
+        Glide.with(activity.applicationContext)
                 .load(R.drawable.oldtimer)
+                .asBitmap()
                 .centerCrop()
-                .into(carImageView)
+                .listener(object : RequestListener<Int, Bitmap> {
+                    override fun onResourceReady(resource: Bitmap?, model: Int?, target: Target<Bitmap>?, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean {
+                        if (resource != null)
+                            Palette.Builder(resource).maximumColorCount(30).generate { palette ->
+                                val swatch = palette.vibrantSwatch ?: palette.mutedSwatch
+                                val contentScrimColor = swatch?.rgb ?: ContextCompat.getColor(activity.applicationContext, android.R.color.holo_purple)
+                                toolbar.setBackgroundColor(contentScrimColor)
+                                toolbar.setStatusBarScrimColor(contentScrimColor)
+                                toolbar.setContentScrimColor(contentScrimColor)
+                                if (swatch != null) {
+                                    toolbar.setCollapsedTitleTextColor(swatch.bodyTextColor)
+                                    toolbar.setExpandedTitleTextAppearance(swatch.bodyTextColor)
+                                }
+                            }
+                        return false
+                    }
 
-        (view.findViewById(R.id.carActions_collapsing_toolbar) as CollapsingToolbarLayout).title = car.name
+                    override fun onException(e: Exception?, model: Int?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                        toolbar.setContentScrimColor(android.R.color.holo_purple)
+                        return false
+                    }
+                })
+                .into(carImageView)
+        toolbar.title = car.name
 
         //Fill car actions list
         val actionPark = CarAction(getString(R.string.carActions_park), resources.getDrawable(R.drawable.ic_add_location, null),
